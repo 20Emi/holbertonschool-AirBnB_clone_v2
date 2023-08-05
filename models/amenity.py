98a907cc009b5b0398268513c94223e0460348d6
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-08-05 16:25:39 -0300

    [10] Fix amenity.py. Add conditional

--- models/amenity.py
+++ models/amenity.py
@@ -4,10 +4,12 @@ from models.base_model import BaseModel, Base
 from models.place import place_amenity
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 class Amenity(BaseModel, Base):
     """"""
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity,
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_amenities = relationship("Place", secondary=place_amenity,
                                     backref="amenities")
