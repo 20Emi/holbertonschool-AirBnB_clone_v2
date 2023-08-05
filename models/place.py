d1d4f7b8261c9bfc5dcf68880869d20745f0d612
20Emi <iop.emilysanchez1@gmail.com>
2023-08-04 17:31:23 -0700

    task 10 of amenities and place

--- models/place.py
+++ models/place.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey(
+                          'places.id'), primary_key=True, nullable=False),
+                      Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -18,8 +24,10 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     # for DBStorage
-    reviews = relationship("Review", backref="place", cascade="all, delete-orphan")
+    reviews = relationship("Review", backref="place",
+                           cascade="all, delete-orphan")
     # for FileStorage
+
     @property
     def reviews(self):
         """Getter for FileStorage
@@ -28,7 +36,31 @@ class Place(BaseModel, Base):
         from models import storage
         reviews_list = []
         for review in storage.all(Place).values():
-            if place.state_id == self.id:
-                reviews_list.append(place)
+            if Place.state_id == self.id:
+                reviews_list.append(Place)
         return reviews_list
     amenity_ids = []
+
+    # for DBStorage task 10
+    amenities = relationship(
+        'Amenity', secondary='place_amenity', viewonly=False, backref="place_amenity")
+
+    # Gette task 10
+    @property
+    def amenities(self):
+        """Getter for FileStorage
+        """
+        from models.amenity import Amenity    # import here to avoid circular import
+        from models import storage
+        amenities_list = []
+        for amenities in storage.all(Amenity).values():
+            if Place.Amenity.id == self.amenity_ids:
+                amenities_list.append(Amenity)
+        return amenities_list
+
+    # Setter task 10
+    @amenities.setter
+    def amenities(self, amenityy):
+        from models.amenity import Amenity
+        if type(amenityy) is Amenity:
+            self.amenity_ids.append(amenityy.id)
