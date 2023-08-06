3e993b5e53cc730469cb7946db084788101d8c11
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-08-06 12:15:41 -0300

    [1] Fixed tests db_storage

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """unnitest of db_storage"""
-import models
-from models.base_model import Base
+from models.base_model import Base, BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
@@ -16,49 +15,75 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.engine.base import Engine
 from models import storage
+from os import getenv
 
 
 class test_DB_Storage(unittest.TestCase):
     """tests"""
 
     @classmethod
-    def up(self):
-        """se esta configurando de la base de datos de prueba
-        y la sesion"""
+    @unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db', "FileStorage Skip")
+    def setUpClass(cls):
+        """Set up the test environment"""
+        cls.storage = DBStorage()
+        cls.storage.reload()  # Load data from the database
+
+    @classmethod
+    @unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db', "FileStorage Skip")
+    def up(cls):
+        """Set up the test environment"""
         if isinstance(models.storage, DBStorage):
-            Base.metadata.create_all(self.storage.__DBStorage__engine)
-            Session = sessionmaker(self.storage.__DBStorage__engine)
-            self.storage._DBStorage__session = self.Session()
-            self.amenity = Amenity(name='EC')
-            self.storage._DBStorage__session.add(self.amenity)
+            Base.metadata.create_all(cls.storage._DBStorage__engine)
+            Session = sessionmaker(cls.storage._DBStorage__engine)
+            cls.storage._DBStorage__session = Session()
 
-            self.city = City(name='EC', state_id=self.state.id)
-            self.storage._DBStorage__session.add(self.city)
+            # Create the User instance first
+            cls.user = User(email='ec@gmail.com', password='root')
+            cls.storage._DBStorage__session.add(cls.user)
 
-            self.place = Place(
-                name='EC', city_id=self.city.id, user_id=self.user.id)
-            self.storage._DBStorage__session.add(self.place)
+            # Create other instances that depend on User
+            cls.state = State(name='EC')
+            cls.storage._DBStorage__session.add(cls.state)
 
-            self.review = Review(
-                text='hola mundo', place_id=self.place.id, user_id=self.user.id)
-            self.storage._DBStorage__session.add(self.review)
+            cls.amenity = Amenity(name='EC')
+            cls.storage._DBStorage__session.add(cls.amenity)
 
-            self.user = User(email='ec@gmail.com', password='root')
-            self.storage._DBStorage__session.add(self.user)
+            # Set cls.state.id before creating the City instance
+            cls.city = City(name='EC', state_id=cls.state.id)
+            cls.storage._DBStorage__session.add(cls.city)
 
-            self.state = State(name='EC')
-            self.storage._DBStorage__session.add(self.state)
+            # Set cls.user.id and cls.city.id before creating the Place
+            cls.place = Place(
+                name='EC', city_id=cls.city.id, user_id=cls.user.id)
+            cls.storage._DBStorage__session.add(cls.place)
 
-    def down(self):
-        self.storage._DBStorage__session.close()
+            # Set cls.place.id and cls.user.id before creating the Review
+            cls.review = Review(
+                text='hola', place_id=cls.place.id, user_id=cls.user.id)
+            cls.storage._DBStorage__session.add(cls.review)
 
+            cls.storage._DBStorage__session.commit()
+
+    @classmethod
+    @unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db', "FileStorage Skip")
+    def tearDownClass(cls):
+        """Tear down the test environment"""
+        cls.storage._DBStorage__session.close()  # Close the database session
+
+    @unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db', "FileStorage Skip")
     def test_new(self):
-        user = User()
+        # Create a new User instance with a valid email and password
+        user = User(email='test@example.com', password='test')
         self.storage.new(user)
         self.assertIn(user, self.storage._DBStorage__session.new)
 
+    @unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db', "FileStorage Skip")
     def test_save(self):
-        user = User()
+        user = User(email='test2@example.com', password='test2')
         self.storage.new(user)
         self.storage.save()
         self.assertIn(user, self.storage._DBStorage__session)
+
+
+if __name__ == '__main__':
+    unittest.main()
