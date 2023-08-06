d2ee5d568291cc857fcc302b54fc56d16a3cf3e4
Cristian Encalada <cris.encalada.camargo@gmail.com>
2023-08-06 13:19:01 -0300

    [1] Simpiify tests in db_storage.py

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -19,7 +19,7 @@ from os import getenv
 
 
 class test_DB_Storage(unittest.TestCase):
-    """tests"""
+    """Class to test the DB storage method"""
 
     @classmethod
     @unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db', "FileStorage Skip")
@@ -30,56 +30,20 @@ class test_DB_Storage(unittest.TestCase):
 
     @classmethod
     @unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db', "FileStorage Skip")
-    def up(cls):
-        """Set up the test environment"""
-        if isinstance(models.storage, DBStorage):
-            Base.metadata.create_all(cls.storage._DBStorage__engine)
-            Session = sessionmaker(cls.storage._DBStorage__engine)
-            cls.storage._DBStorage__session = Session()
-
-            # Create the User instance first
-            cls.user = User(email='ec@gmail.com', password='root')
-            cls.storage._DBStorage__session.add(cls.user)
-
-            # Create other instances that depend on User
-            cls.state = State(name='EC')
-            cls.storage._DBStorage__session.add(cls.state)
-
-            cls.amenity = Amenity(name='EC')
-            cls.storage._DBStorage__session.add(cls.amenity)
-
-            # Set cls.state.id before creating the City instance
-            cls.city = City(name='EC', state_id=cls.state.id)
-            cls.storage._DBStorage__session.add(cls.city)
-
-            # Set cls.user.id and cls.city.id before creating the Place
-            cls.place = Place(
-                name='EC', city_id=cls.city.id, user_id=cls.user.id)
-            cls.storage._DBStorage__session.add(cls.place)
-
-            # Set cls.place.id and cls.user.id before creating the Review
-            cls.review = Review(
-                text='hola', place_id=cls.place.id, user_id=cls.user.id)
-            cls.storage._DBStorage__session.add(cls.review)
-
-            cls.storage._DBStorage__session.commit()
-
-    @classmethod
-    @unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db', "FileStorage Skip")
     def tearDownClass(cls):
         """Tear down the test environment"""
         cls.storage._DBStorage__session.close()  # Close the database session
 
     @unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db', "FileStorage Skip")
-    def test_new(self):
+    def test_new_user(self):
         # Create a new User instance with a valid email and password
-        user = User(email='test@example.com', password='test')
+        user = User(email='user1@example.com', password='password1')
         self.storage.new(user)
         self.assertIn(user, self.storage._DBStorage__session.new)
 
     @unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db', "FileStorage Skip")
-    def test_save(self):
-        user = User(email='test2@example.com', password='test2')
+    def test_save_user(self):
+        user = User(email='user2@example.com', password='password2')
         self.storage.new(user)
         self.storage.save()
         self.assertIn(user, self.storage._DBStorage__session)
